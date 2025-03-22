6d65d227a32630e0d23d37764e8f6ba3b2ebdfbe
lewts-creat <leonardosamuelsilva5@icloud.com>
2025-03-21 21:50:43 -0300

    jogo da adivinhaçao

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 // let subtitulo = document.querySelector('p');
 // subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
 
+let tentativas = 5;
+
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 
@@ -11,18 +13,33 @@ function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10) + 1;
 }
 
-let numeroSecreto = gerarNumeroAleatorio();
+let numeroSecreto = 5
 console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag, texto) {
-    let campo = document.querySelector(tag);
+    let campo = document.querySelector(tag, texto);
     campo.innerHTML = texto;
 }
 
 function verificarChute() {
-    let chute = parseInt(document.querySelector('input').value);
-    console.log(chute === numeroSecreto);
-}
+    if(tentativas >0) {
+        let chute = parseInt(document.querySelector('input').value);
+        tentativas--;
+        if(chute === numeroSecreto) {
+            exibirTextoNaTela('h1', 'Parabens! Voce acertou!');
+        }else {
+            if(tentativas === 0) {
+                exibirTextoNaTela('h1', `Você perdeu!`)
+            }else if(chute < numeroSecreto){
+                exibirTextoNaTela('h1', `Você errou, o número secreto maior, você tem mais ${tentativas} tentativas`)
+            }else {
+                exibirTextoNaTela('h1', `Você errou, o número secreto é menor, você tem mais
+                     ${tentativas} tentativas`)
+            }
+        }
+    }      
+} 
+
 
 //<h1>Jogo de adivinhação</h1>
 //<p>Escolha um número entre 1 e 100</p>
